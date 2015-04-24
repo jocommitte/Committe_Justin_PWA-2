e96a324c65682f0ac7c5ccbe7da3204da70dab4b
jocommitte <jocommitte.com>
2015-04-23 20:13:59 -0400

    toolTip

--- Committe_Justin_PWA2/js/main.js
+++ Committe_Justin_PWA2/js/main.js
@@ -5,9 +5,9 @@
 
 (function($){
 
-	$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+    $('.form').find('input, textarea').on('keyup blur focus', function (e) {
 
-		var $this = $(this),
+        var $this = $(this),
 			label = $this.prev('label');
 
 		if (e.type === 'keyup') {
@@ -48,9 +48,28 @@
 		$(target).fadeIn(600);
 
 	});
-	
-	
-		
+
+
+	$(masterTooltip).hover(function(){
+		var title = $(this).attr('title');
+		$(this).data('tipText', title).removeAttr('title');
+		$("<p class='toolTip'></p>")
+			.text(title)
+			.appendTo('body')
+			.fadeIn('slow');
+	},
+		function(){
+			$(this).attr('title', $(this).data('tipText'));
+			$('.toolTip').remove();
+		}).mousemove(function(e){
+			var mouseX = e.pageX +20;
+			var mouseY = e.pageY +10;
+			$('.toolTip').css({
+				top:mouseY,
+				left:mouseX
+
+			})
+		})
 
 	
 })(jQuery); // end private scope
