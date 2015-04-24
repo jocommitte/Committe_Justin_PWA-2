8b633d98920897091999f00c53666664c94fac05
jocommitte <jocommitte.com>
2015-04-23 23:16:20 -0400

    accordian

--- Committe_Justin_PWA2/js/main.js
+++ Committe_Justin_PWA2/js/main.js
@@ -70,6 +70,18 @@
 
 			})
 		})
+	$('#tabs p').hide().eq(0).show();
+	$('#tabs p:not(:first)').hide();
+
+	$('#tabs-nav li').click(function(e){
+		e.preventDefault();
+		$('#tabs p').hide();
+		$('#tabs-nav .current').removeClass('current');
+		$(this).addClass('current');
+		var clicked = $(this).find('a:first').attr('href');
+		$('#tabs ' + clicked).fadeIn('fast');
+	}).eq(0).addClass('current');
+
 
 	
 })(jQuery); // end private scope
